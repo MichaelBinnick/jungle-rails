5edbb834cc8fb894df3ed0cf08c956164ff1e240
Michael Binnick <mbinnick@live.ca>
2022-10-19 21:05:27 +0000

    implements admindmin dashboard stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book" 
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"] 
 
   def show
+    @admin_product_count = Product.count
+    @admin_category_count = Category.count
   end
 end
